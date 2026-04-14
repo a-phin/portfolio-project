6f50970debae41f44fd16b4f6eeea187411748e1
Angelina Phin <vkpfxfhz48@privaterelay.appleid.com>
2026-04-14 19:39:10 -0400

    Update component to use Song object instead of generic types

---
+++ src/Song.java
@@ -0,0 +1,11 @@
+/**
+ * Song object.
+ *
+ * @param title
+ *            the song name
+ * @param constant
+ *            the song difficulty value
+ */
+public record Song(String title, int constant) {
+
+}
