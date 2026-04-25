c025f9f3fb0377f9f506a861c8fef0ef9e25ca1c
Angelina Phin <vkpfxfhz48@privaterelay.appleid.com>
2026-04-24 23:54:29 -0400

    Simplified not equals to !=, I'm tired of these checkstyle warnings

--- src/components/songselector/SongSelectorSecondary.java
+++ src/components/songselector/SongSelectorSecondary.java
@@ -165,7 +165,7 @@ public abstract class SongSelectorSecondary implements SongSelector {
             int newConstant) {
         assert title != null : "Violation of: title is not null";
         assert newConstant > 0 : "Violation of: constant is greater than 0";
-        assert newConstant != oldConstant : "Violation of: newConstant is not equal to oldConstant";
+        assert newConstant != oldConstant : "Violation of: newConstant != oldConstant";
 
         Iterator<Song> it = this.iterator();
         // Initialize a new song with an empty title and constant 0
